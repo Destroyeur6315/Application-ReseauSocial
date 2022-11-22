23c65a64cdf1eaec83b672c0464ee9e28d7272ce
owargout <Owen.ARGOUT@etu.uca.fr>
2022-11-22 11:45:47 +0100

    ajout session

--- Code/Code-serveur/server.js
+++ Code/Code-serveur/server.js
@@ -5,31 +5,45 @@ const path=require('path');
 var bodyParser = require("body-parser");
 var sessions = require('express-session');
 var crypto = require('crypto');
-const onehour=1000*60*60
+const onehour=1000*60*60;
 
-var generate_key = function() {
-    return crypto.randomBytes(16).toString('base64');
-};
+
 
 // Varibale pour données du formulaire
 // Instancier le serveur
 var serveur = express();
 
+serveur.use(sessions({
+    secret: generate_key,
+    saveUninitialized:true,
+    cookie: { maxAge: onehour },
+    resave: false 
+}));
+
+var session;
+var generate_key = function() {
+    return crypto.randomBytes(16).toString('base64');
+};
 // utilise le module de parsing
-// serveur.use(bodyParser.urlencoded({ extended: true }));
-// serveur.use(sessions({
-//     secret:generate_key(),
-//     cookie: {maxAge :onehour},
-// }))
+serveur.use(bodyParser.urlencoded({ extended: true }));
+serveur.use(sessions({
+    secret:generate_key(),
+    cookie: {maxAge :onehour},
+}))
 
 serveur.get('/',(req,res)=>{
-    // sessions=req.session;
-    // if (sessions.userid){
-    //     res.send("Tu es bien connecté")
-    // }
-    // else
-     res.sendFile(path.resolve(__dirname+'/../Code-FrontEnd/Html/inscription.html'))
+    sessions=req.session;
+    if (sessions.userid){
+        res.send("Tu es bien connecté")
+    }
+    else
+        res.sendFile(path.resolve(__dirname+'/../Code-FrontEnd/Html/inscription.html'))
 })
+
+serveur.get('/deconnection',(req,res) => {
+    req.session.destroy();
+    res.redirect('/');
+});
 
 serveur.get('inscription.html',function(req,res){
     res.sendFile(__dirname + '/publiFormulaire.html');
@@ -39,11 +53,8 @@ serveur.post('/', function(req, res) {
     nom=req.body.name;
     passwd=req.body.pass;
     email=req.body.email;
-    numtel=req.body.numtel;
-
-    console.log("test =" + nomFormulaire);
-    
-    traiter(nomFormulaire);
+    numtel=req.body.numtel;  
+    ajouteruser(nom,passwd,email,numtel);
 });
 
 serveur.post('/publiformulaire.html', function(req, res) {
@@ -61,11 +72,8 @@ serveur.listen(8080, function() {
     console.log('Serveur en écoute...');
 });
 
-function traiter(donne){
-    switch(donne){
-        case "inscription":
-
-            var requete_sql = '\
+function ajouteruser(nom,passwd,email,numtel){
+    var requete_sql = '\
             INSERT INTO user (nom,mot_de_passe,email,numTelephone)'+
             // ici, les valeurs prennent "??" lorsque nous insérons une chaîne de caractères
             // et "?" lorsqu'il s'agit d'un nombre
@@ -91,45 +99,87 @@ function traiter(donne){
             });
 
             con.query(
-            addNewValue,
+            requete_sql,
             function (err, result) {
                 if(err) throw err;
                 console.log(result);
             });
-            break;
-        case "publication":
-            var requete_sql = '\
-            INSERT INTO publication (nomcreateur,contenu,dateecriture)'+
-            // ici, les valeurs prennent "??" lorsque nous insérons une chaîne de caractères
-            // et "?" lorsqu'il s'agit d'un nombre
-            'VALUES(??, ??, NOW())';
-            // l'array "inserts" contient, dans l'ordre d'apparition dans la requête, les éléments
-            // qui doivent remplacer les points d'interrogation
-            var inserts = [
-                nomcreateur,
-                contenu,
-            ];
-                    
-            con.connect(function(err) {   
-                if (err) throw err;   
-                    console.log("Connecté à la base de données MySQL!");   
-                });
-
-            requete_sql = sql.preparer(mysql, requete_sql, inserts);
-            con.query(
-                requete_sql,
-                function(err,result){
-                    if(err) throw err;
-                    console.log(result);}
-            );
-            break;
-        case "connection":
-            
-            break;
-
-    }
 }
 
+  function connexion(pseudo, motDePasse){
+    const con = mysql.createConnection({   host: "localhost",   user: "root",   password: "root",   database : "romain_application" });
+
+    let data;
+
+    // Try to connect
+    con.connect(function(err) {   
+        if (err) throw err;   
+        console.log("Connecté à la base de données MySQL!");  
+    });
+
+    let requete = "SELECT * FROM user WHERE nom = '" + pseudo + "';";
+    console.log(requete);
+
+    var userValide = false;
+
+    function recupResultat(err,  rows, fields) {
+        if (err) throw err;
+        var result = [];
+        for (var i = 0; i < rows.length; i++) {
+                result = rows; //je stock le résultat dans une variable pour l'envoyer à la vue
+                //globalResult.push(i);
+        }
+
+        var test = JSON.stringify(result);
+        var json = JSON.parse(test);
+
+        var mdp = "mot de passe";
+        
+        console.log(json.length);
+        // Gerer les erreurs (quand SELECT ne retourne rien)
+
+        if(json.length > 0){
+            console.log("nom = ", json[0].nom);
+            console.log("mot de passe = ", json[0].motDePasse);
+
+            email = json[0].email;
+
+            console.log(result);
+
+           //var n = comparePassword(motDePasse, json[0].motDePasse);
+
+           bcrypt.compare(motDePasse, json[0].motDePasse, (err, data) => {
+                //if error than throw error
+                if (err) throw err
+                //if both match than you can do anything
+                if (data) {
+                    //return res.status(200).json({ msg: "Login success" })
+                    console.log("mot de passe VALIDE");
+                    valide("valide");
+                } else {
+                    //return res.status(401).json({ msg: "Invalid credencial" })
+                    console.log("mot de passe INVALIDE");
+                    return "invalide";
+                    invalide("invalide");
+                }
+            })
+        }
+
+        return result;
+    }   
+
+    con.query(requete,  recupResultat);
+
+    session=req.session;
+    session.secret=//mettre l'id du user récupéré avec la requète;
+    
+    console.log("OUIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
+
+    con.end(function (err) { 
+        if (err) throw err;
+        else  console.log('Fin de la connexion en BDD'); 
+    });
+}
 // function publication(donne){
 //     var requete_sql = '\
 //   INSERT INTO publication (nomcreateur,contenu,dateecriture)'+
@@ -152,3 +202,70 @@ function traiter(donne){
 //         console.log("Connecté à la base de données MySQL!");   
 //     });
 // }
+
+    // switch(donne){
+    //     case "inscription":
+
+    //         var requete_sql = '\
+    //         INSERT INTO user (nom,mot_de_passe,email,numTelephone)'+
+    //         // ici, les valeurs prennent "??" lorsque nous insérons une chaîne de caractères
+    //         // et "?" lorsqu'il s'agit d'un nombre
+    //         'VALUES(??,??,??,??)';
+    //         // l'array "inserts" contient, dans l'ordre d'apparition dans la requête, les éléments
+    //         // qui doivent remplacer les points d'interrogation
+    //         var inserts = [
+    //             nom,
+    //             passwd,
+    //             email,
+    //             numtel,
+    //         ];
+            
+    //         const mysql = require('mysql');
+
+    //         // Créer la variable pour se connecter à notre base
+    //         const con = mysql.createConnection({   host: "localhost",   user: "root",   password: "root",   database : "romain_application" });  
+
+    //         // Try to connect
+    //         con.connect(function(err) {   
+    //             if (err) throw err;   
+    //             console.log("Connecté à la base de données MySQL!");   
+    //         });
+
+    //         con.query(
+    //         addNewValue,
+    //         function (err, result) {
+    //             if(err) throw err;
+    //             console.log(result);
+    //         });
+    //         break;
+    //     case "publication":
+    //         var requete_sql = '\
+    //         INSERT INTO publication (nomcreateur,contenu,dateecriture)'+
+    //         // ici, les valeurs prennent "??" lorsque nous insérons une chaîne de caractères
+    //         // et "?" lorsqu'il s'agit d'un nombre
+    //         'VALUES(??, ??, NOW())';
+    //         // l'array "inserts" contient, dans l'ordre d'apparition dans la requête, les éléments
+    //         // qui doivent remplacer les points d'interrogation
+    //         var inserts = [
+    //             nomcreateur,
+    //             contenu,
+    //         ];
+                    
+    //         con.connect(function(err) {   
+    //             if (err) throw err;   
+    //                 console.log("Connecté à la base de données MySQL!");   
+    //             });
+
+    //         requete_sql = sql.preparer(mysql, requete_sql, inserts);
+    //         con.query(
+    //             requete_sql,
+    //             function(err,result){
+    //                 if(err) throw err;
+    //                 console.log(result);}
+    //         );
+    //         break;
+    //     case "connection":
+            
+    //         break;
+
+    // }
