ab0e5478794b58fb3588bd75cc41671009f07933
owargout <Owen.ARGOUT@etu.uca.fr>
2022-11-22 16:29:02 +0100

    ajout différent formulaire

--- Code/Code-serveur/server.js
+++ Code/Code-serveur/server.js
@@ -5,6 +5,7 @@ const path=require('path');
 var bodyParser = require("body-parser");
 var sessions = require('express-session');
 var crypto = require('crypto');
+const { Server } = require('http');
 const onehour=1000*60*60;
 
 
@@ -38,7 +39,7 @@ serveur.get('/deconnection',(req,res) => {
 
 serveur.get('inscription.html',function(req,res){
     res.sendFile(__dirname + '/publiFormulaire.html');
-})
+});
 
 serveur.post('/', function(req, res) {
     nom=req.body.name;
@@ -46,6 +47,17 @@ serveur.post('/', function(req, res) {
     email=req.body.email;
     numtel=req.body.numtel;  
     ajouteruser(nom,passwd,email,numtel);
+});
+
+serveur.get('/connexion.html',(req,res)=>{
+    res.sendFile(__dirname + '/../Code-FrontEnd/Html/connexion.html')
+});
+
+Serveur.post('/connexion.html',function(req,res){
+    nom=req.body.name;
+    motdepasse=req.body.pass;
+
+    connexion(nom,motdepasse)
 });
 
 serveur.post('/publiformulaire.html', function(req, res) {
@@ -151,7 +163,6 @@ function ajouteruser(nom,passwd,email,numtel){
                     //return res.status(401).json({ msg: "Invalid credencial" })
                     console.log("mot de passe INVALIDE");
                     return "invalide";
-                    invalide("invalide");
                 }
             })
         }
@@ -167,7 +178,7 @@ function ajouteruser(nom,passwd,email,numtel){
         cookie: { maxAge: onehour },
         resave: false 
     }));
-    session=req.session;
+    session.userid=
     
     console.log("OUIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
 
@@ -176,28 +187,28 @@ function ajouteruser(nom,passwd,email,numtel){
         else  console.log('Fin de la connexion en BDD'); 
     });
 }
-// function publication(donne){
-//     var requete_sql = '\
-//   INSERT INTO publication (nomcreateur,contenu,dateecriture)'+
-//   // ici, les valeurs prennent "??" lorsque nous insérons une chaîne de caractères
-//   // et "?" lorsqu'il s'agit d'un nombre
-//   'VALUES(??, ??, NOW())';
-//     // l'array "inserts" contient, dans l'ordre d'apparition dans la requête, les éléments
-//     // qui doivent remplacer les points d'interrogation
-//     var inserts = [
-//     nomcreateur,
-//     contenu,
-//     ];
+function publication(donne){
+    var requete_sql = '\
+  INSERT INTO publication (nomcreateur,contenu,dateecriture)'+
+  // ici, les valeurs prennent "??" lorsque nous insérons une chaîne de caractères
+  // et "?" lorsqu'il s'agit d'un nombre
+  'VALUES(??, ??, NOW())';
+    // l'array "inserts" contient, dans l'ordre d'apparition dans la requête, les éléments
+    // qui doivent remplacer les points d'interrogation
+    var inserts = [
+    nomcreateur,
+    contenu,
+    ];
 
-//     const mysql = require('mysql');
+    const mysql = require('mysql');
 
-//     const con = mysql.createConnection({   host: "localhost",   user: "root",   password: "root",   database : "romain_application" })
+    const con = mysql.createConnection({   host: "localhost",   user: "root",   password: "root",   database : "romain_application" })
 
-//     con.connect(function(err) {   
-//         if (err) throw err;   
-//         console.log("Connecté à la base de données MySQL!");   
-//     });
-// }
+    con.connect(function(err) {   
+        if (err) throw err;   
+        console.log("Connecté à la base de données MySQL!");   
+    });
+}
 
     // switch(donne){
     //     case "inscription":
