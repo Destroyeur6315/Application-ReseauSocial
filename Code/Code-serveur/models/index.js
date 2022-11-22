d49c5f013ab88a8cd0efe7ca63f4eaf93729faad
Theo RENAUD <Theo.RENAUD@etu.uca.fr>
2022-11-22 10:35:22 +0100

    systeme de chat fonctionnel/ MIME bloque le CSS

--- Code/Code-serveur/models/index.js
+++ Code/Code-serveur/models/index.js
@@ -9,6 +9,15 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
+//Necessaire au système de FAQ
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const { dirname } = require('path');
+const io = new Server(server);
+
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
@@ -36,3 +45,31 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
+
+//Systeme de FAQ
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname+'/../../Code-FrontEnd/Html/faq.html'));
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+  });
+
+server.listen(4000, () => {
+  console.log('listening on *:3000');
+});
