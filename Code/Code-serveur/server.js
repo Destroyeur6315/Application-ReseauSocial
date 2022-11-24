8fe08fe80824e01eeb9fa79ae45ab3e0f214457d
Romain FILLOT <Romain.Fillot@etu.uca.fr>
2022-11-24 17:24:40 +0100

    Inscription fait

--- Code/Code-serveur/server.js
+++ Code/Code-serveur/server.js
@@ -3,10 +3,11 @@ var express = require('express');
 const path = require('path');
 var bodyParser = require("body-parser");
 var sessions = require('express-session');
+const mysql = require('mysql');
 var crypto = require('crypto');
 const { Server } = require('http');
-// var connexionBDD = require('./connection');
 
+// var connexionBDD = require('./connection');
 
 // Definition de variable
 const onehour=1000*60*60;
@@ -19,13 +20,15 @@ var generate_key = function() {
     return crypto.randomBytes(16).toString('base64');
 };
 
+// Création des tables
+createtable();
+
+// Middleware
 // Utilise le module de parsing
 serveur.use(bodyParser.urlencoded({ extended: true }));
 
-
 // Accéder aux Css et aux images
 serveur.use(express.static(path.join(__dirname, '..', 'Code-FrontEnd')));
-
 
 
 // Definition des routes
@@ -46,13 +49,22 @@ serveur.get('/publiformulaire.html',function(req,res){
     res.sendFile(path.resolve(__dirname+'/../Code-FrontEnd/Html/publiformulaire.html'))
 });
 
-
-serveur.post('/', function(req, res) {
-    nom=req.body.name;
+serveur.post('/inscription.html', function(req, res) {
+    pseudo=req.body.pseudo;
     passwd=req.body.pass;
     email=req.body.email;
-    numtel=req.body.numtel;  
-    ajouteruser(nom,passwd,email,numtel);
+    numtel=req.body.numTel;  
+
+    // gérer les erreurs 
+    if(pseudo == "" || passwd == ""){
+         return res.status(400).send("<h1> Erreur : veuillez renseigner un mot de passe et un nom d'utilisater ! <h1>");
+    }
+
+    let idUser=1;
+
+    let requeteAddUser = "INSERT INTO user VALUES('" + idUser + "', '" + pseudo + "' , '" + passwd + "', '" + email + "', '"  + numtel + "');";
+
+    insertUser(requeteAddUser);
 });
 
 /** 
@@ -80,40 +92,6 @@ serveur.listen(8080, function() {
     console.log('Serveur en écoute à l\'adresse suivant : http://localhost:8080/ ...');
 });
 
-function ajouteruser(nom,passwd,email,numtel){
-    var requete_sql = '\
-            INSERT INTO user (nom,mot_de_passe,email,numTelephone)'+
-            // ici, les valeurs prennent "??" lorsque nous insérons une chaîne de caractères
-            // et "?" lorsqu'il s'agit d'un nombre
-            'VALUES(??,??,??,??)';
-            // l'array "inserts" contient, dans l'ordre d'apparition dans la requête, les éléments
-            // qui doivent remplacer les points d'interrogation
-            var inserts = [
-                nom,
-                passwd,
-                email,
-                numtel,
-            ];
-            
-            const mysql = require('mysql');
-
-            // Créer la variable pour se connecter à notre base
-            const con = mysql.createConnection({   host: "localhost",   user: "root",   password: "root",   database : "romain_application" });  
-
-            // Try to connect
-            con.connect(function(err) {   
-                if (err) throw err;   
-                console.log("Connecté à la base de données MySQL!");   
-            });
-            requete_sql=sql.preparer(mysql,requete_sql,inserts)
-
-            con.query(
-            requete_sql,
-            function (err, result) {
-                if(err) throw err;
-                console.log(result);
-            });
-}
 
 function connexion(pseudo, motDePasse){
     const con = mysql.createConnection({   host: "localhost",   user: "root",   password: "root",   database : "romain_application" });
@@ -224,6 +202,74 @@ function publication(donne){
             console.log(result);
         });
 }
+
+function createtable(){
+        const con = mysql.createConnection({   host: "localhost",   user: "root",   password: "root",   database : "romain_application" });
+
+        // Try to connect
+        con.connect(function(err) {   
+            if (err) throw err;     
+        });
+
+        // Requête sql
+        let supprimertable = "DROP TABLE IF EXISTS user;"
+        let createtable = "CREATE TABLE user(\
+                                    id char(5) PRIMARY KEY,\
+                                    nom varchar(100) NOT NULL,\
+                                    motDePasse varchar(100) NOT NULL,\
+                                    email varchar(150),\
+                                    numTelephone varchar(20)\
+                                    );"
+
+        // Lancer les requêtes
+        con.query(
+            supprimertable,
+            function (err, result) {
+                if(err) throw err;
+                console.log(result);
+        });
+
+        con.query(
+            createtable,
+            function (err, result) {
+                if (err) throw err;       
+                console.log(result);    
+        });
+
+        con.end(function (err) { 
+            if (err) throw err;
+            else  console.log('Création de la table USER avec succés.'); 
+        });
+}
+
+function insertUser(requete){
+        const con = mysql.createConnection({   host: "localhost",   user: "root",   password: "root",   database : "romain_application" });
+
+        // Try to connect
+        con.connect(function(err) {   
+            if (err) throw err;   
+            console.log("Connecté à la base de données MySQL!");   
+        });
+        
+        con.query(
+            requete,
+            function (err, result) {
+                if (err) throw err;       
+                console.log(result);    
+        });
+        
+        con.end(function (err) { 
+            if (err) throw err;
+            else  console.log('Nouveau utilisateur ajouté à la BASE DE DONNEES avec succès !'); 
+        });
+}
+
+
+
+
+
+
+
 
     // switch(donne){
     //     case "inscription":
