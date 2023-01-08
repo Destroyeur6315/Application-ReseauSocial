0c2dd596dc7b05ae9ceae1c3fddda04032a5037f
owe5 <113665003+owe5@users.noreply.github.com>
2023-01-08 13:42:04 +0100

    test ajout gestion des images

---
+++ Code/Code-serveur/image-controller.js
@@ -0,0 +1,32 @@
+var multer  = require('multer');
+var imageMiddleware= require('../middlewares/store-images');
+var imageModel= require('../image-models');
+
+module.exports={
+     storeImage:function(req,res){
+        var upload = multer({
+                    storage: imageMiddleware.image.storage(), 
+                    allowedImage:imageMiddleware.image.allowedImage 
+                    }).single('image');
+        upload(req, res, function (err) {
+           if (err instanceof multer.MulterError) {
+              res.send(err);
+           } else if (err) {
+              res.send(err);
+           }else{
+              // store image in database
+               var imageName = req.file.originalname;
+               var inputValues = {
+                  image_name: imageName
+               }
+             // call model
+             imageModel.storeImage(inputValues, function(data){
+               res.render('upload-form',{alertMsg:data})
+             })
+              
+           }
+           
+        })
+        
+     }
+}
