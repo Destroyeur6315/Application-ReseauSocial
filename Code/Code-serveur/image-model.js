d869dbd9530c1d7d242bc941a8d89ce87360f7de
owe5 <113665003+owe5@users.noreply.github.com>
2023-01-08 16:53:07 +0100

    ajout de la route pour les images

--- Code/Code-serveur/image-model.js
+++ Code/Code-serveur/image-model.js
@@ -3,14 +3,14 @@ module.exports={
   storeImage:function(inputValues,callback){
 
   // check unique email address
-var sql='SELECT * FROM images WHERE image_name =?';
+var sql='SELECT image FROM publication WHERE image_name =?';
 db.query(sql,inputValues.image_name,function (err, data, fields) {
  if(err) throw err
  if(data.length>1){
      var msg = inputValues.image_name + " existe déjà";
  }else{ 
     // save users data into database
-    var sql = 'INSERT INTO images SET ?';
+    var sql = 'INSERT INTO publication SET ?';
    db.query(sql, inputValues, function (err, data) {
       if (err) throw err;
    });
